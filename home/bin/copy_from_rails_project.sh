125a98c0668aecf67535f9e0e0708d7f0619ff51
Amiel Martin <amiel@carnesmedia.com>
2011-04-04 14:38:18 -0700

    add copy from rails project script, still need to document

---
+++ home/bin/copy_from_rails_project.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+rails_directory=$(dirname $1)
+rails_file=$(basename $1)
+
+while :; do
+	if test -f $rails_directory/config/boot.rb; then
+		echo "FOUND"
+		break
+	fi
+	
+	if [ $rails_directory == "/" ]; then
+		echo "could not find a nested rails directory"
+		exit 1
+	fi
+		
+	rails_file="$(basename $rails_directory)/$rails_file"
+	rails_directory=$(dirname $rails_directory)
+done
+
+
+
+echo cp -iv $rails_directory/$rails_file $rails_file
+cp -iv $rails_directory/$rails_file $rails_file
+
+
+
