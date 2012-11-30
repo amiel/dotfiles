786582e7949f4312cd0e00343a1d95e80cfc7620
Amiel Martin <amiel@carnesmedia.com>
2012-11-30 12:44:48 -0800

    Some tmux stuff: guard, pomodoro

---
+++ home/guard.rb
@@ -0,0 +1,12 @@
+
+notification(:tmux,
+  :display_message => true,
+  :timeout => 6, # in seconds
+  # :default_message_format => '%s >> %s',
+  # the first %s will show the title, the second the message
+  # Alternately you can also configure *success_message_format*,
+  # *pending_message_format*, *failed_message_format*
+  :line_separator => ' > ', # since we are single line we need a separator
+  :default_message_color  => 'colour0' # 255'
+  # :color_location => 'status-right-bg' # to customize which tmux element will change color
+)
