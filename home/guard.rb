6467e74f0606ee8c9e91dc5110359b62ab217162
Amiel Martin <amiel.martin@gmail.com>
2013-10-07 11:54:04 -0700

    Stop blowing up when there is no guard/shell

--- home/guard.rb
+++ home/guard.rb
@@ -3,12 +3,17 @@ notification(:file, path: '.guard_result')
 
 # On start => false?
 ::Bundler.with_clean_env do
-  guard(:shell) do
-    watch('.guard_result') do
-      ::Bundler.with_clean_env do
-        `digicolor $(head -1 .guard_result) --blink`
+  begin
+    require 'guard/shell'
+    guard(:shell) do
+      watch('.guard_result') do
+        ::Bundler.with_clean_env do
+          `digicolor $(head -1 .guard_result) --blink`
+        end
       end
     end
+  rescue LoadError
+    # TODO: A warning message?
   end
 end
 
