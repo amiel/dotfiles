86fd8fefe8a67b10fbd97347d630e0aad01e2a3e
Amiel Martin <amiel.martin@gmail.com>
2013-10-10 23:16:07 -0700

    More tmux color improvements

--- home/guard.rb
+++ home/guard.rb
@@ -17,16 +17,11 @@ notification(:file, path: '.guard_result')
   end
 end
 
-notification(:tmux,
-             :display_message => true,
-             :display_message => false,
-             :timeout => 6, # in seconds
-             :default_message_format => '%s >> %s',
-             # the first %s will show the title, the second the message
-             # Alternately you can also configure *success_message_format*,
-             # *pending_message_format*, *failed_message_format*
-             :line_separator => ' > ', # since we are single line we need a separator
-             :default_message_color  => 'colour0' # 255'
-# :color_location => 'status-right-bg' # to customize which tmux element will change color
-            )
+notification(:tmux, {
+  :display_message => false,
+  :success => 'colour150',
+  :failed => 'colour174',
+  :pending => 'colour179',
+  :color_location => %w[status-left-bg pane-active-border-fg pane-border-fg],
+}) if ENV['TMUX']
 
