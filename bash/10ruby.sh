bf0603e95b32eff8050f613a665e3a530139b789
Amiel Martin <amiel@actionsprout.com>
2016-02-24 13:59:09 -0800

    Set up PATH for heroku toolbelt and rails 4 binstubs

--- bash/10ruby.sh
+++ bash/10ruby.sh
@@ -8,6 +8,9 @@ if [ ! -z $BREW_PREFIX ];then
   RUBY_CONFIGURE_OPTS="--with-readline-dir='$BREW_PREFIX'"
 fi
 
+# Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+
 # Use bundle binstubs first.
 # NOTE: This can be considered a security issue.
-# export PATH="./bin:$PATH"
+export PATH="./bin:$PATH"
