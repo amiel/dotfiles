b64f39620828d674dfe41d3dd8945e01a6955cef
Amiel Martin <amiel@actionsprout.com>
2017-04-27 07:41:45 -0800

    Make yarn path first

--- bash/10node.sh
+++ bash/10node.sh
@@ -1,12 +1,11 @@
 export NVM_DIR="$HOME/.nvm"
 
-
 if __command_exists brew; then
   . "$(brew --prefix nvm)/nvm.sh"
 fi
 
 if __command_exists yarn; then
-  export PATH="$PATH:`yarn global bin`"
+  export PATH="`yarn global bin`:$PATH"
 fi
 
 export NPM_AUTH_TOKEN="$(head -n 1 ~/.npmrc | sed "s/\/\/.*=//")"
