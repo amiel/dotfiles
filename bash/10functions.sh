c786fdb3fe7b09bbc14444818d914b2196872bf6
Amiel Martin <amiel@actionsprout.com>
2017-04-27 07:32:02 -0800

    Add tmw bash function for long running side-tasks

--- bash/10functions.sh
+++ bash/10functions.sh
@@ -2,3 +2,12 @@
 function __command_exists {
   hash $1 2>&-
 }
+
+# Split a temporary long-running process
+#
+# For example:
+#
+#   tmw watch df -h
+function tmw {
+  tmux split-window -dvb -p 30 "$*"
+}
