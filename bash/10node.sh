d1642ac7b3587412f95d33330d04ae400d62e506
Amiel Martin <amiel@actionsprout.com>
2017-10-20 19:33:53 -0700

    Prevent using .npmrc if it does not exist

--- bash/10node.sh
+++ bash/10node.sh
@@ -1,11 +1,13 @@
 export NVM_DIR="$HOME/.nvm"
 
+# if __command_exists yarn; then
+#   export PATH="`yarn global bin`:$PATH"
+# fi
+
 if __command_exists brew; then
   . "$(brew --prefix nvm)/nvm.sh"
 fi
 
-if __command_exists yarn; then
-  export PATH="`yarn global bin`:$PATH"
+if [ -e ~/.nmprc ];then
+  export NPM_AUTH_TOKEN="$(head -n 1 ~/.npmrc | sed "s/\/\/.*=//")"
 fi
-
-export NPM_AUTH_TOKEN="$(head -n 1 ~/.npmrc | sed "s/\/\/.*=//")"
